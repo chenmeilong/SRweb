9b3fd100a48b77e69faf1d39f7a250deb16f01bf
ChenMeilong <chemeilong1998@foxmail.com>
2023-08-21 16:30:08 +0800

    updata

--- 07VUE/3.vue3/02组合式APIsetup/setup-demo/src/App.vue
+++ 07VUE/3.vue3/02组合式APIsetup/setup-demo/src/App.vue
@@ -1,61 +1,50 @@
 <template>
-	<h2>当前求和为：{{sum}}</h2>
-	<button @click="sum++">点我+1</button>
-	<hr>
-	<h2>当前的信息为：{{msg}}</h2>
-	<button @click="msg+='！'">修改信息</button>
-	<hr>
+	<h1>一个人的信息</h1>
 	<h2>姓名：{{person.name}}</h2>
 	<h2>年龄：{{person.age}}</h2>
-	<h2>薪资：{{person.job.j1.salary}}K</h2>
-	<button @click="person.name+='~'">修改姓名</button>
-	<button @click="person.age++">增长年龄</button>
-	<button @click="person.job.j1.salary++">涨薪</button>
+	<h3>工作种类：{{person.job.type}}</h3>
+	<h3>工作薪水：{{person.job.salary}}</h3>
+	<h3>爱好：{{person.hobby}}</h3>
+	<h3>测试的数据c：{{person.job.a.b.c}}</h3>
+	<button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-	import {ref,reactive,watch} from 'vue'
+	import {reactive} from 'vue'
 	export default {
 		name: 'App',
 		setup(){
 			//数据
-			let sum = ref(0)
-			let msg = ref('你好啊')
 			let person = reactive({
 				name:'张三',
 				age:18,
 				job:{
-					j1:{
-						salary:20
+					type:'前端工程师',
+					salary:'30K',
+					a:{
+						b:{
+							c:666
+						}
 					}
-				}
-			})
-			console.log(person)
-			watch(sum,(newValue,oldValue)=>{
-				console.log('sum的值变化了',newValue,oldValue)
+				},
+				hobby:['抽烟','喝酒','烫头']
 			})
 
-			//ref监听对象监听不到数据改变的解决办法
-			//法一 deep:true 因为ref会把对象数据自动转换成reactive对象使用proxy
-			// watch(person,(newValue,oldValue)=>{
-			// 	console.log('person的值变化了',newValue,oldValue)
-			// },{deep:true})
-			//法二 person.value是一个reactive对象
-			// watch(person.value,(newValue,oldValue)=>{
-			// 	console.log('person的值变化了',newValue,oldValue)
-			// },)
-
-      // reactive
-			// watch(person,(newValue,oldValue)=>{
-			// 	console.log('person的值变化了',newValue,oldValue)
-			// },{deep:false})
-
+			//方法
+			function changeInfo(){
+				person.name = '李四'
+				person.age = 48
+				person.job.type = 'UI设计师'
+				person.job.salary = '60K'
+				person.job.a.b.c = 999
+				person.hobby[0] = '学习'
+				console.log(person.hobby.indexOf('烫头'));
+			}
 
 			//返回一个对象（常用）
 			return {
-				sum,
-				msg,
-				person
+				person,
+				changeInfo
 			}
 		}
 	}
